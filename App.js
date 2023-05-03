4252d2dfb308dbf82959fc30e41e77238463441d
Olayinka Oniyide <oniyideolayinka97@gmail.com>
2023-05-03 15:36:28 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MovieSpace from './pages/dashboard/component/MovieSpace';
+function App() {
+  return (
+
+     <div className="App">
+        <MovieSpace/>
+  </div>
+  );
+}
+ 
+export default App;
